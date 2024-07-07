56ff09f4ebd7694729b640c13272ac1ab51ab6ed
duston <>
2024-07-06 23:13:14 -0500

    silly

--- src/routes/projects/+page.ts
+++ src/routes/projects/+page.ts
@@ -4,19 +4,24 @@ const current: App.Project = {
     title: "🧍 Current Project",
     description: "Something about dungeons and crawling them",
     date: "May 25 2024",
+    link: "",
+    image: "",
 };
 
 const mir: App.Project = {
     title: "🛑 Madness Interactive Reloaded",
     description: "I worked on various parts of MIR, but more notably: improved upon/created dev tools and editors, lots of bugfixing, and writing tests and documentation.",
-    date: "March 28 2023"
+    date: "March 28 2023",
+    link: "",
+    image: "",
 }
 
 const pjp23: App.Project = {
     title: "🌸 Project BLOOM",
     description: "Made for PJP Game Jam 2023-2. I teamed up with 2 complete strangers to create a game in 3 days. You cultivate plants on a space station to produce oxygen and sell for market.",
+    date: "November 2023",
     link: "https://itch.io/jam/pjp-game-jam-2023-2",
-    date: "November 2023"
+    image: "",
 }
 
 export const load: PageLoad = async ({ params }) => {
